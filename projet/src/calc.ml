c2cdbf38fe36ce76be8e02662eecafa10f87a9a8
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2023-04-06 18:00:24 +0200

    Menu

--- projet/src/calc.ml
+++ projet/src/calc.ml
@@ -6,7 +6,24 @@ let rec eval expr =
   | App2 (Plus, e1, e2) -> eval e1 + eval e2
   | _ -> failwith "Not implemented"
 
-let () =
-  let ast = Parser.expr Lexer.token (Lexing.from_channel stdin) in
+let menu_evaluate str =
+  let ast = Parser.expr Lexer.token (Lexing.from_channel str) in
   let result = eval ast in
   print_int result; print_newline ()
+
+let rec menu () =
+  print_endline "Choose an action:";
+  print_endline "1. Evaluate";
+  print_endline "2. Do something else";
+  print_endline "0. Exit";
+  match read_int () with
+  | 0 -> print_endline "Exiting..."
+  | 1 -> 
+    print_endline "Evaluate : ";
+    menu_evaluate stdin;
+    menu ()
+  | 2 -> print_endline "Performing action 2"; menu ()
+  | _ -> print_endline "Invalid choice"; menu ()
+  
+let () =
+  menu ()
