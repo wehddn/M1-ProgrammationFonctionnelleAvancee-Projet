0c8869183ecdfd744a38b0ec6757bd67211b91ea
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2023-04-06 17:38:24 +0200

    Added eval

--- projet/src/calc.ml
+++ projet/src/calc.ml
@@ -1,2 +1,12 @@
-let ast = Parser.expr Lexer.token (Lexing.from_channel stdin)
-in print_string (Syntax.to_string  ast); print_newline ()
+open Syntax
+
+let rec eval expr =
+  match expr with
+  | Num n -> n
+  | App2 (Plus, e1, e2) -> eval e1 + eval e2
+  | _ -> failwith "Not implemented"
+
+let () =
+  let ast = Parser.expr Lexer.token (Lexing.from_channel stdin) in
+  let result = eval ast in
+  print_int result; print_newline ()
