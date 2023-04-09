756582f04379b69476542c0a3e09d4dd223f7363
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2023-04-09 19:23:37 +0200

    Added trigonometric rule

--- projet/src/simpl.ml
+++ projet/src/simpl.ml
@@ -19,13 +19,20 @@ let simpl_arith expr =
 
   | _ -> expr
 
+let simpl_trig expr =
+  match expr with  
+  (* tan(x) = sin(x)/cos(x) *)
+  | App1 (Tan, e) -> App2 (Div, App1 (Sin, e), App1 (Cos, e))
+  | _ -> expr
+
 
 let rec simpl_aux expr =
   let expr' = simpl_arith expr in
+  let expr' = simpl_trig expr' in 
   let expr2 = if expr' = expr then expr else simpl_aux expr' in
   match expr2 with
   | Num n -> Num n
-  | Var v -> failwith "Variable"
+  | Var v -> Var v
   | App0 _ -> failwith "App0"
   | App1 (op, e) -> App1 (op, simpl_aux e)
   | App2 (op, e1, e2) -> App2 (op, simpl_aux e1, simpl_aux e2)
