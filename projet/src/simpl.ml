5c69b183dbcf47f0445ef54c047609e1cf66e1c1
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2023-04-10 16:43:02 +0200

    Update simpl

--- projet/src/simpl.ml
+++ projet/src/simpl.ml
@@ -1,47 +1,16 @@
 open Syntax
 open Eval
 
-type tree_simpl =
-  | E
-  | T of (expr * expr list)
-
-let tree_add_expr tree expr = 
-  match tree with
-    | E -> tree
-    | T (value, list) -> T (value, list@[expr])
-
-let rec tree_add_exprs tree exprs = 
-  match exprs with
-    | [] -> tree
-    | x::t -> tree_add_exprs (tree_add_expr tree x) t
-
-let tree_add_tree tree tree' = 
-  match tree with
-    | E -> tree
-    | T (value, list) -> T (value, list@[tree'])
-
-let tree_to_string tree =
-  match tree with
-  | E -> "E"
-  | T (value, sub_trees) ->
-      let sub_tree_strings = List.map Syntax.to_string sub_trees in
-      let sub_tree_string = String.concat ", " sub_tree_strings in
-      Printf.sprintf "T (%s, [%s])" (Syntax.to_string value) sub_tree_string
-
-let expr_from_tree tree =
-  match tree with
-    | E -> []
-    | T (expr, list) -> expr::list
-
-let remove_duplicates tree =
-  match tree with
-  | T (value, lst) -> T (value, List.sort_uniq compare lst)
-  | E -> tree
-
 let simpl_arith expr =
   match expr with
   (* x - x = 0 *)
   | App2 (Minus, e1, e2) when e1 = e2 -> Num 0
+
+  (* x - 0 = x *)
+  | App2 (Minus, e1, e2) when e2 = Num 0 -> e1 
+
+  (* x - 0 = x *)
+  | App2 (Minus, e1, e2) when e1 = Num 0 -> App1 (UMinus, e2)
 
   (* a * 0 = 0 *)
   | App2 (Mult, e1, e2) when e1 = Num 0 || e2 = Num 0 -> Num 0 
@@ -60,41 +29,98 @@ let simpl_trig expr =
   match expr with  
   (* tan(x) = sin(x)/cos(x) *)
   | App1 (Tan, e) -> App2 (Div, App1 (Sin, e), App1 (Cos, e))
+  (* tan(x) = sin(x)/cos(x) *)
+  | App2 (Div, App1 (Sin, e1), App1 (Cos, e2)) when e1 = e2 -> App1 (Tan, e1)
   | _ -> expr
 
-let replace_e1 op e2 value_list = 
+let replace2_e1 op e2 value_list = 
   List.map (fun value -> App2 (op, value, e2)) value_list 
 
-let replace_e2 op e1 value_list = 
+let replace1_e1 op value_list = 
+  List.map (fun value -> App1 (op, value)) value_list 
+
+let replace2_e2 op e1 value_list = 
   List.map (fun value -> App2 (op, e1, value)) value_list 
 
-let rec simpl_aux expr = 
-  let tree = T (expr, []) in
-  let expr' = simpl_arith expr in
-  let tree = if expr' = expr then tree else tree_add_expr tree expr' in
-  let expr' = simpl_trig expr in
-  let tree = if expr' = expr then tree else tree_add_expr tree expr' in
-  print_endline (tree_to_string tree);
-  match expr with
-  | Num _ -> tree
-  | Var _ -> tree
-  | App0 _ -> tree 
-  | App1 _ -> tree
-  | App2 (op, e1, e2) ->
-    let e1_tree = simpl_aux e1 in
-    let e1_expr_list = expr_from_tree e1_tree in
-    let res1_list = replace_e1 op e2 e1_expr_list in
-    let tree = tree_add_exprs tree res1_list in
+let replace2_e1_e2 op e1_lst e2_lst = 
+  List.concat (List.map (fun e1 -> List.map (fun e2 -> App2 (op, e1, e2)) e2_lst) e1_lst)
 
-    let e2_tree = simpl_aux e2 in
-    let e2_expr_list = expr_from_tree e2_tree in
-    let res2_list = replace_e2 op e1 e2_expr_list in
-    let tree = tree_add_exprs tree res2_list in
+let lst_expr_to_string lst_expr =
+  match lst_expr with
+  | sub_lst_exprs ->
+    let sub_lst_expr_strings = List.map Syntax.to_string sub_lst_exprs in
+    let sub_lst_expr_string = String.concat ", " sub_lst_expr_strings in
+    Printf.sprintf "%s" sub_lst_expr_string
+
+let remove_duplicates lst_expr =
+  match lst_expr with
+  | lst -> List.sort_uniq compare lst
+
+let simplify expr =
+  let expr_arith = simpl_arith expr in
+  let expr_trig = simpl_trig expr in
+  let expr_lst = if expr = expr_arith then [expr] else [expr_arith] in
+  let expr_lst = if expr = expr_trig then expr_lst@[] else expr_lst@[expr_trig] in
+  expr_lst
+
+let rec simpl_aux expr = 
+  match expr with
+  | Num _ -> [expr]
+  | Var _ -> [expr]
+  | App0 _ -> [expr] 
+  | App1 (op, e) -> 
+    let e_lst = simplify e in
+
+    let e1_simpl = List.map (fun expr -> simpl_aux expr) e_lst |> List.concat in
+
+    let res_list = replace1_e1 op e1_simpl in
+    let res_list = remove_duplicates res_list in
+    res_list
+
+  | App2 (op, e1, e2) ->
+    let e1_lst = simplify e1 in
+
+    let e2_lst = simplify e2 in
+
+    let e1_simpl = List.map (fun expr -> simpl_aux expr) e1_lst |> List.concat in
+    let e2_simpl = List.map (fun expr -> simpl_aux expr) e2_lst |> List.concat in
+
+    let res1_list = replace2_e1 op e2 e1_simpl in
+    let res2_list = replace2_e2 op e1 e2_simpl in
+    let res3_list = replace2_e1_e2 op e1_simpl e2_simpl in
+    let res_list = res1_list@res2_list@res3_list in
+
+    let res_list = remove_duplicates res_list in
     
-    let tree = remove_duplicates tree in
-    tree
+    res_list
+
+let rec count_nodes expr =
+  match expr with
+  | Var _ -> 1
+  | Num _ -> 1
+  | App0 _ -> 1
+  | App1 (_, e) -> 1 + count_nodes e
+  | App2 (_, e1, e2) -> 1 + count_nodes e1 + count_nodes e2
+
+let shortest_expr exprs =
+  let rec helper min_expr min_count = function
+    | [] -> min_expr
+    | e :: es ->
+      let count = count_nodes e in
+      if count < min_count then helper e count es
+      else helper min_expr min_count es
+  in
+  match exprs with
+  | [] -> None
+  | e :: es -> Some (helper e (count_nodes e) es)
 
 let simpl expr =
-  let tree = simpl_aux expr in 
-  print_endline (tree_to_string tree);
-   expr
+  let expr_lst = simplify expr in
+
+  let lst_expr = List.map (fun expr -> simpl_aux expr) expr_lst |> List.concat in
+  let lst_expr = List.map (fun expr -> simplify expr) lst_expr |> List.concat in
+
+  let result = shortest_expr lst_expr in
+  match result with
+  | None -> expr
+  | Some result -> result
