726839f10f913a837513dac3c4659d611e132e9f
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2023-04-14 18:12:25 +0200

    Added normalization; simpl refactored

--- projet/src/simpl.ml
+++ projet/src/simpl.ml
@@ -1,5 +1,5 @@
 open Syntax
-open Eval
+open Norm
 
 (*
   The code consists of two main parts, the first one is a set of
@@ -13,13 +13,12 @@ let simpl_arith expr =
   (* - - x = x *)
   | App1 (UMinus, App1 (UMinus, e1)) -> e1
 
-  (* - x + y = y - x *)
-  | App2 (Plus, App1 (UMinus, e1), e2) -> App2 (Minus, e2, e1)
+  (* x + (-y) = y - x *)
+  | App2 (Plus, e1, App1 (UMinus, e2)) -> App2 (Minus, e1, e2)
 
-  (* x + 0 = 0*)
+  (* 0 + x = 0*)
   | App2 (Plus, e1, e2) -> 
-    if e1 = Num 0 then e2 else
-    if e2 = Num 0 then e1 else expr
+    if e1 = Num 0 then e2 else expr
 
   (* x - x = 0 *)
   | App2 (Minus, e1, e2) when e1 = e2 -> Num 0
@@ -30,19 +29,18 @@ let simpl_arith expr =
   (* 0 - x = -x *)
   | App2 (Minus, e1, e2) when e1 = Num 0 -> App1 (UMinus, e2)
 
-  (* x * 0 = 0 *)
-  | App2 (Mult, e1, e2) when e1 = Num 0 || e2 = Num 0 -> Num 0
+  (* 0 * x = 0 *)
+  | App2 (Mult, e1, e2) when e1 = Num 0 -> Num 0
 
-  (* x * 1 = x *)
+  (* 1 * x = x *)
   | App2 (Mult, e1, e2) -> 
-    if e1 = Num 1 then e2 else
-    if e2 = Num 1 then e1 else expr
+    if e1 = Num 1 then e2 else expr
 
   (* x / 1 = x *)
   | App2 (Div, e1, e2) when e2 = Num 1 -> e1
 
-  (* 0 / x = x *)
-  | App2 (Div, e1, e2) when e1 = Num 0 -> e2
+  (* 0 / x = 0 *)
+  | App2 (Div, e1, e2) when e1 = Num 0 && e2 <> Num 0 -> Num 0
 
   (* log(exp(x)) = x *)
   | App1 (Log, App1 (Exp, e)) -> e 
@@ -53,50 +51,34 @@ let is_even num = num mod 2 = 0
 
 let simpl_trig expr =
   match expr with
-  (* tan(x) = sin(x)/cos(x) *)
+  (* sin(x)/cos(x) = tan(x) *)
   | App2 (Div, App1 (Sin, e1), App1 (Cos, e2)) when e1 = e2 -> App1 (Tan, e1)
 
-  (* sin(a)*cos(b) + cos(a)*sin(b) = sin(a+b) and combinations *)
-  | App2 (Plus, App2 (Mult, App1 (Sin, a1), App1 (Cos, b1)), App2 (Mult, App1 (Cos, a2), App1 (Sin, b2)))
-  | App2 (Plus, App2 (Mult, App1 (Sin, a1), App1 (Cos, b1)), App2 (Mult, App1 (Sin, b2), App1 (Cos, a2)))  
-  | App2 (Plus, App2 (Mult, App1 (Cos, b1), App1 (Sin, a1)), App2 (Mult, App1 (Sin, b2), App1 (Cos, a2)))
-  | App2 (Plus, App2 (Mult, App1 (Cos, b1), App1 (Sin, a1)), App2 (Mult, App1 (Cos, a2), App1 (Sin, b2)))
+  (* cos(a)*sin(b)+cos(b)*sin(a) = sin(a+b) *)
+  | App2 (Plus, App2 (Mult, App1 (Cos, a1), App1 (Sin, b1)), App2 (Mult, App1 (Cos, b2), App1 (Sin, a2)))
     when a1 = a2 && b1 = b2 -> App1 (Sin, (App2 (Plus, a1, b1)))
 
-  (* sin(a)*cos(b) - cos(a)*sin(b) = sin(a-b) and combinations *)
-  | App2 (Minus, App2 (Mult, App1 (Sin, a1), App1 (Cos, b1)), App2 (Mult, App1 (Cos, a2), App1 (Sin, b2)))
-  | App2 (Minus, App2 (Mult, App1 (Sin, a1), App1 (Cos, b1)), App2 (Mult, App1 (Sin, b2), App1 (Cos, a2)))
-  | App2 (Minus, App2 (Mult, App1 (Cos, b1), App1 (Sin, a1)), App2 (Mult, App1 (Sin, b2), App1 (Cos, a2)))
+  (* cos(b)*sin(a)-cos(a)*sin(b) = sin(a-b) *)
   | App2 (Minus, App2 (Mult, App1 (Cos, b1), App1 (Sin, a1)), App2 (Mult, App1 (Cos, a2), App1 (Sin, b2)))
     when a1 = a2 && b1 = b2 -> App1 (Sin, (App2 (Minus, a1, b1)))
 
-  (* cos(a)*cos(b) - sin(a)*sin(b) = cos(a+b) and combinations *)
+  (* cos(a)*cos(b)-sin(a)*sin(b) = cos(a+b) *)
   | App2 (Minus, App2 (Mult, App1 (Cos, a1), App1 (Cos, b1)), App2 (Mult, App1 (Sin, a2), App1 (Sin, b2)))
     when a1 = a2 && b1 = b2 -> App1 (Cos, (App2 (Plus, a1, b1)))
-  | App2 (Minus, App2 (Mult, App1 (Cos, a1), App1 (Cos, b1)), App2 (Mult, App1 (Sin, b2), App1 (Sin, a2)))
-    when a1 = a2 && b1 = b2 -> App1 (Cos, (App2 (Plus, a1, b1)))
 
-  (* cos(a)*cos(b) + sin(a)*sin(b) = cos(a-b) and combinations *)
+  (* cos(a)*cos(b) + sin(a)*sin(b) = cos(a-b) *)
   | App2 (Plus, App2 (Mult, App1 (Cos, a1), App1 (Cos, b1)), App2 (Mult, App1 (Sin, a2), App1 (Sin, b2)))
     when a1 = a2 && b1 = b2 -> App1 (Cos, (App2 (Minus, a1, b1)))
-  | App2 (Plus, App2 (Mult, App1 (Cos, a1), App1 (Cos, b1)), App2 (Mult, App1 (Sin, b2), App1 (Sin, a2)))
-    when a1 = a2 && b1 = b2 -> App1 (Cos, (App2 (Minus, a1, b1)))
   
-  (* (tan(a) + tan(b))/(1 - tan(a)*tan(b)) = tan(a+b) and combinations *)
+  (* (tan(a) + tan(b))/(1 - tan(a)*tan(b)) = tan(a+b) *)
   | App2 (Div, App2 (Plus, App1 (Tan, a1), App1 (Tan, b1)), App2 (Minus, Num 1, App2 (Mult, App1 (Tan, a2), App1 (Tan, b2))))
-    when a1 = a2 && b1 = b2 -> App1 (Tan, (App2 (Plus, a1, b1)))
-  | App2 (Div, App2 (Plus, App1 (Tan, a1), App1 (Tan, b1)), App2 (Minus, Num 1, App2 (Mult, App1 (Tan, b2), App1 (Tan, a2))))
-    when a1 = a2 && b1 = b2 -> App1 (Tan, (App2 (Plus, a1, b1)))
-  | App2 (Div, App2 (Plus, App1 (Tan, b1), App1 (Tan, a1)), App2 (Minus, Num 1, App2 (Mult, App1 (Tan, a2), App1 (Tan, b2))))
     when a1 = a2 && b1 = b2 -> App1 (Tan, (App2 (Plus, a1, b1)))
 
   (* (tan(a) - tan(b))/(1 + tan(a)*tan(b)) = tan(a-b) *)
   | App2 (Div, App2 (Minus, App1 (Tan, a1), App1 (Tan, b1)), App2 (Plus, Num 1, App2 (Mult, App1 (Tan, a2), App1 (Tan, b2))))
     when a1 = a2 && b1 = b2 -> App1 (Tan, (App2 (Minus, a1, b1)))
-  | App2 (Div, App2 (Minus, App1 (Tan, a1), App1 (Tan, b1)), App2 (Plus, Num 1, App2 (Mult, App1 (Tan, b2), App1 (Tan, a2))))
-    when a1 = a2 && b1 = b2 -> App1 (Tan, (App2 (Minus, a1, b1)))
 
-  (* sin(x+2π) = sin(x) *)
+  (* sin(x+2*pi) = sin(x) *)
   | App1 (op, App2 (Plus, e, App2 (Mult, Num n, App0 (Pi)))) 
     when is_even n && (op = Sin || op = Cos) -> App1 (op, e)
 
@@ -134,6 +116,7 @@ let rec count_nodes expr =
   smallest number of nodes.
 *)
 let simpl expr =
+  let expr = norm expr in
   let rec aux expr node_count =
     let res = simpl_aux expr in
     let new_count = count_nodes res in
