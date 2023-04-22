a98ed784a07779324cde8d74b0c0b7425858999c
GRAFF nicolas <nicolas.graff@etu.u-paris.fr>
2023-04-22 15:46:50 +0200

    Update integ

--- projet/src/integ2.ml
+++ projet/src/integ2.ml
@@ -1,8 +1,14 @@
 open Syntax
-open Eval
-open Simpl
+open Primitive
 open Subst
-open Derive
+open Eval
   
-let integ (expr : expr) (x : string) (a : float) (b : float) : expr =
-  expr
+let formule pexpr (x : string) a b = 
+  let saexpr = subst pexpr x a in
+  let sbexpr = subst pexpr x b in
+  eval sbexpr -. eval saexpr 
+
+let integ (expr : expr) (x : string) (a : expr) (b : expr) : float =
+  let pexpr = primitive expr (Light.(Var x)) in
+  let res = if expr <> pexpr then formule pexpr x a b else 0.
+  in res
