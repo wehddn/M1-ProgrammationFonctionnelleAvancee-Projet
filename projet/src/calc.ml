5526a122c1c7c32cf662d5fe055f4ef0e1b514ad
zeggari <h.zeggari7@gmail.com>
2023-05-12 21:46:31 +0200

    régler menu

--- projet/src/calc.ml
+++ projet/src/calc.ml
@@ -7,20 +7,47 @@ open Plot
 open Graphics
 
 
+let last_result = ref None
+
 let parse_expression str =
   Parser.expr Lexer.token (Lexing.from_channel str)
 
 let menu_evaluate str =
-  let ast = parse_expression str in
-  let result = eval ast in
-  print_float result; print_newline ()
+  let _ = match !last_result with
+  | Some result ->
+    print_string "Résultat de la dernière opération : ";
+    print_string (to_string result);
+    print_newline ()
+  | None -> ()  (* Aucun dernier résultat disponible *)
+  in
+    let ast = parse_expression str in
+    let result = eval ast in
+    print_float result;
+    print_newline ()
+  
+
 
 let menu_simpl str =
+  let _ = match !last_result with
+  | Some result ->
+    print_string "Résultat de la dernière opération : ";
+    print_string (to_string result);
+    print_newline ()
+  | None -> ()  (* Aucun dernier résultat disponible *)
+  in
   let ast = parse_expression str in
   let result = simpl ast in
+  last_result := Some result;  (* Stocker le dernier résultat *)
   print_string (to_string result); print_newline ()
 
 let menu_subst str =
+  let _ = match !last_result with
+  | Some result ->
+    print_string "Résultat de la dernière opération : ";
+    print_string (to_string result);
+    print_newline ()
+  | None -> ()  (* Aucun dernier résultat disponible *)
+  in
   print_endline "Saisir l'expression à remplacer";
   let e = Parser.expr Lexer.token (Lexing.from_channel str) in
   print_endline "Entrer la variable à remplacer";
@@ -28,36 +55,52 @@ let menu_subst str =
   print_endline "Saisir l'expression remplaçante";
   let e' = Parser.expr Lexer.token (Lexing.from_channel str) in
   let result = subst e x e' in
+  last_result := Some result;  (* Stocker le dernier résultat *)
   print_string (to_string result); print_newline ()
-
+  
 let menu_derive str =
+  let _ = match !last_result with
+  | Some result ->
+    print_string "Résultat de la dernière opération : ";
+    print_string (to_string result);
+    print_newline ()
+  | None -> ()  (* Aucun dernier résultat disponible *)
+  in
   print_endline "Saisir l'expression à dériver";
   let e = Parser.expr Lexer.token (Lexing.from_channel str) in
   print_endline "Entrer la variable par rapport à laquelle dériver";
   let x = read_line () in
   let result = derive e x in
+  last_result := Some result;  (* Stocker le dernier résultat *)
   print_string (to_string result); print_newline ()
 
-  let menu_plot str =
-    print_endline "Entrez l'expression à tracer:";
-    let e = Parser.expr Lexer.token (Lexing.from_channel str) in
-    print_endline "Entrez la variable:";
-    let x = read_line () in
-    print_endline "Voulez-vous entrer des intervalles personnalisés pour les axes ? (o/n)";
-    let custom_intervals = read_line () in
-    if custom_intervals = "o" || custom_intervals = "O" then (
-      print_endline "Entrez la valeur minimale de l'axe des x:";
-      let x_min = read_float () in
-      print_endline "Entrez la valeur maximale de l'axe des x:";
-      let x_max = read_float () in
-      print_endline "Entrez la valeur minimale de l'axe des y:";
-      let y_min = read_float () in
-      print_endline "Entrez la valeur maximale de l'axe des y:";
-      let y_max = read_float () in
-      plot_expression e x x_min x_max y_min y_max
-    ) else (
-      plot_expression e x (-5.0) 5.0 (-5.0) 5.0
-    )
+let menu_plot str =
+  let _ = match !last_result with
+  | Some result ->
+    print_string "Résultat de la dernière opération : ";
+    print_string (to_string result);
+    print_newline ()
+  | None -> ()  (* Aucun dernier résultat disponible *)
+  in
+  print_endline "Entrez l'expression à tracer:";
+  let e = Parser.expr Lexer.token (Lexing.from_channel str) in
+  print_endline "Entrez la variable:";
+  let x = read_line () in
+  print_endline "Voulez-vous entrer des intervalles personnalisés pour les axes ? (o/n)";
+  let custom_intervals = read_line () in
+  if custom_intervals = "o" || custom_intervals = "O" then (
+    print_endline "Entrez la valeur minimale de l'axe des x:";
+    let x_min = read_float () in
+    print_endline "Entrez la valeur maximale de l'axe des x:";
+    let x_max = read_float () in
+    print_endline "Entrez la valeur minimale de l'axe des y:";
+    let y_min = read_float () in
+    print_endline "Entrez la valeur maximale de l'axe des y:";
+    let y_max = read_float () in
+    plot_expression e x x_min x_max y_min y_max
+  ) else (
+    plot_expression e x (-5.0) 5.0 (-5.0) 5.0
+  )
   
 
 
