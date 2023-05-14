8e99c08b8f1f015ad46783a1e3580b1b482bf01b
zeggari <h.zeggari7@gmail.com>
2023-05-14 15:09:00 +0200

    régler ^

--- projet/src/derive.ml
+++ projet/src/derive.ml
@@ -35,4 +35,6 @@ let rec derive e x =
        | Mult -> App2 (Plus, App2 (Mult, derive e1 x, e2), App2 (Mult, e1, derive e2 x))
        | Minus -> App2 (Minus, derive e1 x, derive e2 x)
        | Div -> App2 (Div, App2 (Minus, App2 (Mult, derive e1 x, e2), App2 (Mult, e1, derive e2 x)), App2 (Expo, e2, Num 2))
-       | Expo -> App2 (Mult, e, App2 (Plus, App2 (Mult, derive e2 x, App1 (Log, e1)), App2 (Div, App2 (Mult, derive e1 x, e2), e1))))
+       | Expo -> App2 (Mult, e2, App2 (Expo, e1, App2 (Minus, e2, Num 1)))
+      )
+        
