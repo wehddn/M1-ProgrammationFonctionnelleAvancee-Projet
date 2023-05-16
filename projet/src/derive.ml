28712554b5b8ff4cb6c6cba32017c4fb55ee961c
graff <nicolas.graff@etu.u-paris.fr>
2023-05-16 17:24:31 +0200

    Added simplification to derive

--- projet/src/derive.ml
+++ projet/src/derive.ml
@@ -3,7 +3,7 @@ open Eval
 open Simpl
 open Subst
 
-let rec derive e x =
+let rec derive_aux e x =
   match e with
   (* e est une variable = x, la dérivée est 1 *)
   | Var x' when x = x' -> Num 1
@@ -18,23 +18,24 @@ let rec derive e x =
   (* e est une opération unaire *)
   | App1 (op1, e1) ->
       (match op1 with
-       | Sqrt -> App2 (Div, derive e1 x, App2 (Mult, Num 2, App1 (Sqrt, e1)))
-       | Exp -> App2 (Mult, derive e1 x, e)
-       | Log -> App2 (Div, derive e1 x, e1)
-       | Sin -> App2 (Mult, derive e1 x, App1 (Cos, e1))
-       | Cos -> App2 (Mult, derive e1 x, App1 (UMinus, App1 (Sin, e1)))
-       | Tan -> App2 (Mult, derive e1 x, App2 (Plus, Num 1, App2 (Expo, App1 (Tan, e1), Num 2)))
-       | ASin -> App2 (Div, derive e1 x, App1 (Sqrt, App2 (Minus, Num 1, App2 (Expo, e1, Num 2))))
-       | ACos -> App2 (Div, App1 (UMinus, derive e1 x), App1 (Sqrt, App2 (Minus, Num 1, App2 (Expo, e1, Num 2))))
-       | ATan -> App2 (Div, derive e1 x, App2 (Plus, Num 1, App2 (Expo, e1, Num 2)))
-       | UMinus -> App1 (UMinus, derive e1 x))
+       | Sqrt -> App2 (Div, derive_aux e1 x, App2 (Mult, Num 2, App1 (Sqrt, e1)))
+       | Exp -> App2 (Mult, derive_aux e1 x, e)
+       | Log -> App2 (Div, derive_aux e1 x, e1)
+       | Sin -> App2 (Mult, derive_aux e1 x, App1 (Cos, e1))
+       | Cos -> App2 (Mult, derive_aux e1 x, App1 (UMinus, App1 (Sin, e1)))
+       | Tan -> App2 (Mult, derive_aux e1 x, App2 (Plus, Num 1, App2 (Expo, App1 (Tan, e1), Num 2)))
+       | ASin -> App2 (Div, derive_aux e1 x, App1 (Sqrt, App2 (Minus, Num 1, App2 (Expo, e1, Num 2))))
+       | ACos -> App2 (Div, App1 (UMinus, derive_aux e1 x), App1 (Sqrt, App2 (Minus, Num 1, App2 (Expo, e1, Num 2))))
+       | ATan -> App2 (Div, derive_aux e1 x, App2 (Plus, Num 1, App2 (Expo, e1, Num 2)))
+       | UMinus -> App1 (UMinus, derive_aux e1 x))
   (* e est une opération binaire *)
   | App2 (op2, e1, e2) ->
       (match op2 with
-       | Plus -> App2 (Plus, derive e1 x, derive e2 x)
-       | Mult -> App2 (Plus, App2 (Mult, derive e1 x, e2), App2 (Mult, e1, derive e2 x))
-       | Minus -> App2 (Minus, derive e1 x, derive e2 x)
-       | Div -> App2 (Div, App2 (Minus, App2 (Mult, derive e1 x, e2), App2 (Mult, e1, derive e2 x)), App2 (Expo, e2, Num 2))
+       | Plus -> App2 (Plus, derive_aux e1 x, derive_aux e2 x)
+       | Mult -> App2 (Plus, App2 (Mult, derive_aux e1 x, e2), App2 (Mult, e1, derive_aux e2 x))
+       | Minus -> App2 (Minus, derive_aux e1 x, derive_aux e2 x)
+       | Div -> App2 (Div, App2 (Minus, App2 (Mult, derive_aux e1 x, e2), App2 (Mult, e1, derive_aux e2 x)), App2 (Expo, e2, Num 2))
        | Expo -> App2 (Mult, e2, App2 (Expo, e1, App2 (Minus, e2, Num 1)))
       )
         
+let derive e x = simpl (derive_aux e x)
